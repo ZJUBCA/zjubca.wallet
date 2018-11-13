0b6106b518586716b8777fa641c4bad360a7535d
awmleer <awmleer@sparker.xyz>
2018-11-13 16:44:52 +0800

    upgrade packages

--- App.js
+++ App.js
@@ -36,6 +36,7 @@ export default class App extends React.Component {
       Font.loadAsync({
         // This is the font that we are using for our tab bar
         ...Icon.Ionicons.font,
+        ...Icon.FontAwesome.font,
         // We include SpaceMono because we use it in HomeScreen.js. Feel free
         // to remove this if you are not using it in your app
         'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
